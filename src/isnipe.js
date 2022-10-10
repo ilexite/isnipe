e002e651f2461ffd70c93a1f08e9918d80fb7826
ilexite <ilexite1@gmail.com>
2022-10-10 20:09:06 +0100

    Added dump command for debugging

--- src/isnipe.js
+++ src/isnipe.js
@@ -2,11 +2,34 @@ const paths = require("./paths");
 const bootstrapper = require("./bootstrapper");
 const SharedData = require("./sharedData");
 
+const package = require("../package.json");
+
 const fs = require("node:fs/promises");
 const fss = require("node:fs");
 const path = require("node:path");
+const os = require("node:os");
 
 const ini = require("ini");
+const { Command } = require("commander");
+
+const program = new Command();
+
+program
+	.name(package.name)
+	.description(package.description)
+	.version(package.version);
+
+program
+	.command("dump")
+	.description("Dump data. Useful for debugging.")
+	.action(() => dump());
+
+program
+	.command("run")
+	.description("You know what it does.")
+	.action(() => main());
+
+program.parse();
 
 async function main() {
 	const sd = new SharedData();
@@ -30,6 +53,21 @@ async function main() {
 
 	process.on("SIGINT", stop);
 	process.on("SIGTERM", stop);
+}
+
+async function dump() {
+	console.log("== Begin Info ==");
+	console.log("Version: %s", package.version);
+	console.log("OS: %s", os.platform());
+	console.log("Arch: %s", os.arch());
+	console.log("Kernel: %s", os.version());
+	console.log("== End Info ==");
+
+	const config = await preprocess(await readini(paths.configFile));
+
+	console.log("== Begin Config ==");
+	console.log(config);
+	console.log("== End Config ==");
 }
 
 async function readini(file) {
@@ -61,5 +99,3 @@ async function preprocess(obj) {
 
 	return copy;
 }
-
-main();
